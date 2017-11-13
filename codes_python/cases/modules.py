7caf914162db7931ec4ab9337d2197aca885b249
nsaura <nsaura@bef5c912-3013-4295-81d9-13fa92c63195>
2017-11-13 17:57:47 +0000

    Begining

--- codes_python/cases/modules.py
+++ codes_python/cases/modules.py
@@ -17,25 +17,15 @@ def test (**kwargs) :
     
     print dico['thrd']
 
-N_discr=50; T_inf=50 
-line_z = np.linspace(0.,1.,N_discr)
-T_n =  list(map(lambda x : -4*T_inf*x*(x-1), line_z[1:N_discr-1]))
 
-df = pd.DataFrame(T_n)    
-df.to_csv("T.csv", index=False, header=True)
+X_init = 0; X_fin = 5 ; NN = 150
 
-T_read = pd.read_csv("T.csv").get_values()
-T_read = T_read.reshape(T_read.shape[0])
+dx = np.abs(X_init - X_fin)/float(NN)
+der= []
+f = lambda X : X*X
 
-lii = line_z[1:N_discr-1]
-
-plt.ion()
-plt.figure("writen and read")
-plt.plot(lii, T_read, label='read', marker='o')
-plt.plot(lii, T_n, label='written')
-plt.legend(loc='best')
-plt.show() 
+for i in np.arange(X_init, X_fin+dx, dx) :
+    der.append((f(i+dx) - f(i))/dx)
     
-
 
 
