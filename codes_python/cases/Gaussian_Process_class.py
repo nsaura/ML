6d308b04aca71c0b9262626d1a51a0c55bd46094
nsaura <nsaura@bef5c912-3013-4295-81d9-13fa92c63195>
2018-02-11 02:04:06 +0000

    ML SF

--- codes_python/cases/Gaussian_Process_class.py
+++ codes_python/cases/Gaussian_Process_class.py
@@ -126,36 +126,34 @@ def maximize_LML(T, N_sample): #Rajouter variances
     
     phi_op = phi(h_op)
     
-    return h_op, phi_op, 
+    phi_var_inv = np.linalg.inv(phi_op + var_mat)
+    
+    return h_op, phi_var_inv 
 
-def ML(T, X_train, Y_train, var, phi_op, h_op, N_sample, x_s) :
+def ML(T, X_train, Y_train, var, phi_var_inv, h_op, N_sample, x_s) :
     """
     h ici est supposée optimizée pour maximiser la correspondance entre les prédictions sur Y_train et la fonction de covariance phi
     x_s l'entrée dont on veut prédire la sortie
     """
     N = N_sample*(T.N_discr-2)*len(T.T_inf_lst)
-
     if len(np.shape(var)) == 1 :
         var *= np.eye(N)
     
     var_mat = var * np.eye(N)
-    
-    phi_var_inv = np.linalg.inv(phi_op + var_mat)
-    
     phi_vec_s = np.asarray([np.exp(-np.linalg.norm(x_s - X, 2)**2 / h_op**2) for X in X_train]) ## Vecteur de taille N
     
     ## Vecteur de taille N 
     alpha = np.dot(phi_var_inv, Y_train).reshape(N)
     
     beta_ML = alpha.dot(phi_vec_s) 
-        
     sigma_ML = 1 - phi_vec_s.dot(phi_var_inv.dot(phi_vec_s))
+
     return (beta_ML, sigma_ML)
     
 
 def test(T, N_sample, T_inf) :
     X_train, Y_train, var = training_set(T, N_sample)
-    h_op, phi_op = maximize_LML(T, N_sample)
+    h_op, phi_var_inv = maximize_LML(T, N_sample)
     
     sT_inf = "T_inf_%d" %(T_inf)
     Tdistrib = T.h_beta(T.bfgs_adj_bmap[sT_inf] + T.bfgs_adj_cholesky[sT_inf].dot(T.tab_normal(0,1,T.N_discr-2)[0]), T_inf)
@@ -168,7 +166,7 @@ def test(T, N_sample, T_inf) :
     beta, sigma = [],[] 
     
     for x in X_test :
-        res = ML(T, X_train, Y_train, var, phi_op, h_op, N_sample, x)
+        res = ML(T, X_train, Y_train, var, phi_var_inv, h_op, N_sample, x)
         beta.append(res[0])
         sigma.append(res[1] + var_moy)
     
@@ -180,7 +178,7 @@ def test(T, N_sample, T_inf) :
     T_max = T.h_beta(beta_span[1], T_inf)   
         
     plt.figure("Temperature beta learning")
-    plt.plot(T.line_z, T.h_beta(beta, T_inf), label="Learning N_sample %d" %(N_sample), marker='o', color='r', fillstyle = 'none', linestyle='None')
+    plt.plot(T.line_z, T.h_beta(beta, T_inf), label="Learning N_sample %d" %(N_sample), marker='o', c='r', fillstyle = 'none', linestyle='None')
     plt.plot(T.line_z, T.h_beta(T.bfgs_adj_bmap[sT_inf], T_inf), label="Inference" , marker='+', color='green', fillstyle = 'none', linestyle='None')
     plt.plot(T.line_z, Tdistrib, label="True", color='k', linestyle='--')
     plt.fill_between(T.line_z, T_min, T_max, facecolor= "1", alpha=0.7, interpolate=True, hatch='/', color="grey")
@@ -232,46 +230,103 @@ def True_Temp(T, T_inf, body) :
     
     return T_nNext_obs
     
-def 
-    
-def test_2(T, N_sample, T_inf) :
+def True_Beta(T, T_inf) :
+    """
+    Pour comparer beta_true et beta_ML
+    """
+    t1 = np.asarray([ 1./T.eps_0*(1. + 5.*np.sin(3.*np.pi/200. * T[i]) + np.exp(0.02*T[i])) *10**(-4) for i in range(T.N_discr-2)])
+    t2 = np.asarray([T.h / T.eps_0*(T_inf[i] - T[i])/(T_inf[i]**4 - T[i]**4)  for i in range(N_discr-2)]) 
+
+#*************#
+#*************#
+#*************#
+
+def solver_ML(T, N_sample, T_inf, body):
     X_train, Y_train, var = training_set(T, N_sample)
-    h_op, phi_op = maximize_LML(T, N_sample)
+    h_op, phi_var_inv = maximize_LML(T, N_sample)
     
-    sT_inf = "T_inf_%d" %(T_inf)
-    Tdistrib = T.h_beta(T.bfgs_adj_bmap[sT_inf] + T.bfgs_adj_cholesky[sT_inf].dot(T.tab_normal(0,1,T.N_discr-2)[0]), T_inf)
+    T_inf_old = T_inf
+    T_inf = map(T_inf, T.line_z) 
+    
     var_moy = np.mean(var)
     
-    X_test = np.zeros((1,2))
-    for t in Tdistrib :
-        X_test = np.block([[X_test], [T_inf, t]])    
-    X_test = np.delete(X_test, 0, axis=0)    
-    beta, sigma = [],[] 
-    
-    for x in X_test :
-        res = ML(T, X_train, Y_train, var, phi_op, h_op, N_sample, x)
-        beta.append(res[0])
-        sigma.append(res[1] + var_moy)
-    
-    beta_span = []
-    beta_span.append([beta[i] + sigma[i] for i in range(len(beta))])
-    beta_span.append([beta[i] - sigma[i] for i in range(len(beta))])
+    T_n = T_nmspan = T_nMspan = np.asarray(map(lambda x : -4*T_inf[(T.N_discr-2)/2]*x*(x-1), T.line_z) )
+    beta_span, beta = [[0], [0]], []
+    sigma = []
 
-    T_min = T.h_beta(beta_span[0], T_inf) 
-    T_max = T.h_beta(beta_span[1], T_inf)   
+    for j,t in enumerate(T_n) :
+        x_s = np.array([T_inf[j], t]) ## T_inf, T(z)
+        res = ML(T, X_train, Y_train, var, phi_var_inv, h_op, N_sample, x_s)
+        beta.append(res[0])
+        sigma.append(res[1] + var_moy )
+    beta_span[0] = [beta[i] - sigma[i] for i in range(len(beta))]
+    beta_span[1] = [beta[i] + sigma[i] for i in range(len(beta))]
+    
+    B_n = np.zeros((T.N_discr-2, 1))
+    B_nmspan   =   np.zeros((T.N_discr-2, 1)) # Calcul des incertitudes (m pour min)
+    B_nMspan   =   np.zeros((T.N_discr-2, 1)) # Calcul des incertitudes (M pour max)
+    
+    beta_n = np.asarray(beta)
+    
+    T_n_tmp = T_mn_tmp = T_Mn_tmp = np.zeros((T.N_discr-2, 1))
+    tol ,err, compteur, cmax = 1e-4, 1.0, 0, 800 
+    
+    while (np.abs(err) > tol) and (compteur <= cmax) :
+        if compteur > 0 :
+            T_n = T_nNext
+            T_nmspan = T_nmNext
+            T_nMspan = T_nMNext
+
+            beta_n = beta_nNext
+            beta_span[0] = [beta[i] - sigma[i] for i in range(len(beta))]
+            beta_span[1] = [beta[i] + sigma[i] for i in range(len(beta))]
+ 
+            beta, sigma = [], []
+        compteur +=1 
+            
+        T_n_tmp = np.dot(T.A2, T_n)
+        T_mn_tmp = np.dot(T.A2, T_nmspan)
+        T_Mn_tmp = np.dot(T.A2, T_nMspan)
         
-    plt.figure("Temperature beta learning")
-    plt.plot(T.line_z, T.h_beta(beta, T_inf), label="Learning N_sample %d" %(N_sample), marker='o', color='r', fillstyle = 'none', linestyle='None')
-    plt.plot(T.line_z, T.h_beta(T.bfgs_adj_bmap[sT_inf], T_inf), label="Inference" , marker='+', color='green', fillstyle = 'none', linestyle='None')
-    plt.plot(T.line_z, Tdistrib, label="True", color='k', linestyle='--')
-    plt.fill_between(T.line_z, T_min, T_max, facecolor= "1", alpha=0.7, interpolate=True, hatch='/', color="grey")
-    plt.legend(loc="best")
-    plt.show()
+        for i in range(T.N_discr-2) :
+#            try :
+            B_n[i]      = T_n_tmp[i] + T.dt*(beta_n[i])*T.eps_0*(T_inf[i]**4 - T_n[i]**4)
+            B_nmspan[i] = T_mn_tmp[i] + T.dt*(beta_span[0][i])*T.eps_0*(T_inf[i]**4 - T_nmspan[i]**4)
+            B_nMspan[i] = T_Mn_tmp[i] + T.dt*(beta_span[1][i])*T.eps_0*(T_inf[i]**4 - T_nMspan[i]**4)
+          
+        T_nNext = np.dot(np.linalg.inv(T.A1), B_n)
+        T_nmNext = np.dot(np.linalg.inv(T.A1), B_nmspan)
+        T_nMNext = np.dot(np.linalg.inv(T.A1), B_nMspan)
+        
+        for j,t in enumerate(T_n) :
+            x_s = np.array([T_inf[j], t]) ## T_inf, T(z)
+            res =  ML(T, X_train, Y_train, var, phi_var_inv, h_op, N_sample, x_s)
+            beta.append(res[0])
+            sigma.append(res[1] + var_moy)
+        beta_nNext = np.asarray(beta)
+
+        err = np.linalg.norm(T_nNext - T_n, 2) # Norme euclidienne
     
-    plt.figure("Comparaison Beta learning VS inference")
-    plt.plot(T.line_z, beta, label= "learning")
-    plt.plot(T.line_z, T.bfgs_adj_bmap[sT_inf], label="inference")
+    print ("Calculus with {} completed. Convergence status :".format(body))
+    print ("Err_obs = {} ".format(err))    
+    print ("Iterations = {} ".format(compteur))
+
+    T_true = True_Temp(T, T_inf_old, body)
+    
+    n = T.N_discr-2
+    T_true = T_true.reshape(n)
+    T_nNext = T_nNext.reshape(n)
+    T_nmNext= T_nmNext.reshape(n)
+    T_nMNext= T_nMNext.reshape(n)
+    
+    plt.figure("Beta_True vs Beta_ML; N_sample = {}; T_inf = {}".format(N_sample, body)) 
+    plt.plot(T.line_z, T_true, label="True T_field for T_inf={}".format(body), c='k', linestyle='--')
+    plt.plot(T.line_z, T_nNext, label="ML T_field".format(body), marker='o', fillstyle='none', linestyle='none', c='r')
+    plt.fill_between(T.line_z, T_nmNext, T_nMNext, facecolor= "1", alpha=0.7, interpolate=True, hatch='/', color="grey")
     plt.legend()
-    plt.show()
     
-    return beta, sigma
+    return T_true, T_nNext
+        
+    
+
+
