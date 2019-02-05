9255d0a2016e015919a63aefb756e4b2454faf76
Nathaniel Saura <saura.nathaniel@gmail.com>
2019-02-05 18:27:11 +0100

    brgrsder

--- codes_python/keras/brgrs_der_direc1D.py
+++ codes_python/keras/brgrs_der_direc1D.py
@@ -40,153 +40,23 @@ solvers = reload(solvers)
 
 from visu_strides import glide1D
 
+import time
+
 try :
     tf.reset_default_graph()
     print ("Graph reseted")
 except :
     pass
 
-#def model(Nx, first_filter) :
-#    # Unet 
-#    input_shape = (Nx,1)
-
-#    input_data = Input(shape=input_shape)
-#    x = Conv1D(first_filter, kernel_size=3, padding='same')(input_data)
-#    #Rajouter une couche BN
-#    x = Activation('relu')(x)
-#    print ("in --> Conv1D(32, 2, same) = {}".format(x.get_shape())) # (?, 81, 32)
-
-#    x = Conv1D(first_filter*2, kernel_size=2, padding='same')(input_data)
-#    x = Activation('relu')(x)   
-#    print ("Conv1D --> Conv1D(32, 2, same). Out_shape = {} \n".format(x.get_shape()))
-#    #print x.get_shape() # (?, 81, 32)
-
-#    in_pool_shape = x.get_shape()
-
-#    x = MaxPool1D(pool_size=2, padding='same')(x)
-#    print ("MaxPool1D pool_size = 2, padding same")
-#    print ("MaxPool1D(1st enc): in_shape = {}\tOut_shape = {}".format(in_pool_shape, x.get_shape()))
-#    print (" ")
-#    # 
-
-#    x = Conv1D(first_filter*4, kernel_size=2, padding='same')(x)
-#    #Rajouter une couche BN
-#    x = Activation('relu')(x)
-#    print ("Pooled --> Conv1D(64, 2, same). Out_shape = {}".format(x.get_shape()))
-
-#    x = Conv1D(first_filter*8, kernel_size=2, padding='same')(x)
-#    #Rajouter une couche BN
-#    x = Activation('relu')(x)
-#    print ("Conv1D --> Conv1D(64, 2, same). Out_shape = {} \n".format(x.get_shape()))
-
-#    in_pool_shape = x.get_shape()
-
-#    x = MaxPool1D(pool_size=2, padding='same')(x)
-#    print ("MaxPool1D pool_size = 2, padding same")
-#    print ("MaxPool1D(2nd enc): in_shape = {}\tOut_shape = {}".format(in_pool_shape, x.get_shape()))
-#    print (" ")
-
-#    x = Conv1D(first_filter*16, kernel_size=2, padding='same')(x)
-#    #Rajouter une couche BN
-#    x = Activation('relu')(x)
-#    print ("Pooled --> Conv1D(128, 2, same). Out_shape = {}".format(x.get_shape()))
-
-#    x = Conv1D(first_filter*16, kernel_size=2, padding='same')(x)
-#    #Rajouter une couche BN
-#    x = Activation('relu')(x)
-#    print ("Conv1D --> Conv1D(128, 2, same). Out_shape = {} \n".format(x.get_shape()))
-
-#    in_samp_shape = x.get_shape()
-
-#    x = UpSampling1D(2)(x)
-#    print ("Upsampling pool_size = 2")
-#    print ("UpSampling(1st dec): in_shape= {}\tOut_shape = {}".format(in_samp_shape, x.get_shape()))
-#    print (" ")
-
-#    x = Conv1D(first_filter*8, kernel_size=2, padding='same')(x)
-#    #Rajouter une couche BN
-#    x = Activation('relu')(x)
-#    print ("Upsampled --> Conv1D(64, 2, same). Out_shape = {}".format(x.get_shape()))
-
-#    x = Conv1D(first_filter*4, kernel_size=2, padding='same')(x)
-#    #Rajouter une couche BN
-#    x = Activation('relu')(x)
-#    print ("Conv1D --> Conv1D(64, 2, same). Out_shape = {} \n".format(x.get_shape()))
-
-#    in_samp_shape = x.get_shape()
-
-#    x = UpSampling1D(2)(x)
-#    print ("Upsampling pool_size = 2")
-#    print ("UpSampling(2nd dec): in_shape= {}\tOut_shape = {}".format(in_samp_shape, x.get_shape()))
-#    print (" ")
-
-#    x = Conv1D(first_filter*2, kernel_size=3, padding='valid')(x)
-#    #Rajouter une couche BN
-#    x = Activation('relu')(x)
-#    print ("Upsampled --> Conv1D(32, 2, same). Out_shape = {}".format(x.get_shape()))
-
-#    x = Conv1D(1, kernel_size=1, padding='valid')(x)
-#    #Rajouter une couche BN
-#    output = Activation('relu')(x)
-#    print ("Conv1D --> Conv1D(1, 1, same) (Output). Out_shape = {}".format(x.get_shape()))
-
-#    unet = Model(input_data, output, name='u_net_out')
-#    
-#    
-#    return unet
-##- - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
-#- - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-# u_it139_0_Nt52_Nx82_CFL0_4_nu0_05_sin.npy
-
-#def build_dataset(roe=roe) :
-#    data_file = csv.reader(open("./../cases/data/roe/roe_inference_case_done.txt","r"), delimiter="\t")
-#    lines = []
-#    
-#    for line in data_file :
-#        lines.append(line)
-#    
-#    n_params = np.shape(lines)[1]
-#    n_cases = np.shape(lines)[0] - 1 
-#    
-#    p = [[lines[j][i].replace(' ','') for j in range(n_cases)] for i in range(n_params)]
-#    
-#    for l in range(len(p)) :
-#        for j in range(1, n_case + 1) : #+1 because key is still in lists
-#            try :
-#                p[l][j] = p[l][j]
-#            except :
-#                pass
-#    
-#    keys = []
-#    cases = []
-#    
-#    keys = [p[j][0] for j in range(n_params)]
-#    cases = [p[j][1:] for j in range(n_params)] 
-#    
-#    print np.shape(p)
-#    
-    
-    
-#    for c in range(n_cases) :
-#        for it in range(200) :
-#            for ind, kk in enumerate(keys[:-1]) :
-#                if kk == 'init' :
-#                    ll += "sin_" 
-#                else :
-#                    try :
-#                        ll +='%s%s_' %(kk, v[ind][c])
-#                    except :
-#                        print "kk = {}\tind={}\tc={}".format(kk, ind, c)
-#                        print "ll = {}".format(ll)
-#                        return v[ind]   
-#            ll +="u_it%d_0.npy" % it
-
-#    Nx250_Nt100_CFL0_40_amp1_00_U_adv0_30_sin_harm1_00_phase0_00_u_it58_5.npy
-
 def build_dataset(roe) :
     X = np.zeros((roe.Nx - 2)) # roe.Nx
     y = np.zeros_like(range(roe.Nx - 2)) # roe.Nx
+    
+    final_X = np.zeros((roe.Nx - 2)) # roe.Nx
+    final_y = np.zeros((roe.Nx - 2)) # roe.Nx
+    final_d = np.zeros((roe.Nx - 2)) # roe.Nx
+    
+    deru = np.zeros((roe.Nx - 2))
     
     print "X.shape init : {}".format(X.shape)
     
@@ -203,12 +73,14 @@ def build_dataset(roe) :
     calc_deri = lambda field : np.array([(field[j+1] - field[j-1]) / (2. * dx) \
                                          for j in range(1, roe.Nx - 1)]).ravel()
     
+    times = []
+    
     CFL = '0_40'
     for amp in ['0_50', '1_00', '1_50']:
         for u in ['0_30', '0_60', '0_90', '1_20', '1_50'] :
-            for h in ['1_00', '2_00', '3_00'] :
+            for h in ['1_00' , '2_00', '3_00'] :
                 for ph in ['0_00', '0_17', '0_33', '0_50'] :
-                    for it in range(0, 182, 5) :
+                    for it in range(180) :
                         
                         # For X                    
                         ffile = header_file(amp, u, init, h, ph, it, 0)
@@ -227,38 +99,64 @@ def build_dataset(roe) :
                         # Effective build of X and y 
 #                        print curr_u[1:-1]
 #                        print X
+                        
+                        time1 = time.time()
                         X = np.block([ [X], [curr_u[1:-1]] ]) #, curr_der] ])
                         y = np.block([ [y], [next_u[1:-1]] ])
+                        deru = np.block([ [deru], [curr_der] ])
+                        
+                        times.append(abs(time.time() - time1))
+                        
+                        if cnt % 500 == 0 :
+                            print cnt
                         cnt += 1
-    
+                        
+                    final_X = np.block([ [final_X], [X] ])
+                    final_y = np.block([ [final_y], [y] ])
+                    final_d = np.block([ [final_d], [deru] ])
+
+                    X = np.zeros((roe.Nx - 2))
+                    y = np.zeros((roe.Nx - 2))
+                    deru = np.zeros((roe.Nx - 2))
+                
     print cnt
     
-    X = np.delete(X, 0, axis=0)
-    y = np.delete(y, 0, axis=0)
-    return X, y 
+    final_X = np.delete(final_X, 0, axis=0)
+    final_y = np.delete(final_y, 0, axis=0)
+    final_d = np.delete(final_d, 0, axis=0)
+    
+    final_X = np.delete(final_X, 0, axis=0)
+    final_y = np.delete(final_y, 0, axis=0)
+    final_d = np.delete(final_d, 0, axis=0)
+    
+    return final_X, final_d, final_y 
     
 #    for j in range(1, len(self.line_x)-1)
     
 #    return X, y, (cnt, X_keys, y_keys, u_means)
 
-def split_data(X, y):
+def split_data(X, der, y):
     permutation = np.random.permutation(len(y))
     
     X, y = np.copy(X), np.copy(y)
     
     X_rand = X[permutation]
     y_rand = y[permutation]
+    der_rand = der[permutation]
     
     xtr = X_rand[:int(len(y)*0.8)] 
     ytr = y_rand[:int(len(y)*0.8)]
+    dertr = der_rand[:int(len(y)*0.8)]    
     
     xte = X_rand[int(len(y)*0.8):int(len(y)*0.9)]
     yte = y_rand[int(len(y)*0.8):int(len(y)*0.9)]
-    
+    derte = der_rand[int(len(y)*0.8):int(len(y)*0.9)]
+        
     xval = X_rand[int(len(y)*0.9):]
     yval = y_rand[int(len(y)*0.9):]
+    derval = der_rand[int(len(y)*0.9):]
     
-    return xtr, ytr, xte, yte, xval, yval
+    return xtr, ytr, dertr, xte, yte, derte, xval, yval, derval
 
 def see_pred(roe, unet, u_means, key, indice) :
     u_n = xval[indice]
@@ -272,10 +170,9 @@ def see_pred(roe, unet, u_means, key, indice) :
     
 #    u_nexact = 
     
-    
 
 if __name__ == '__main__':
-    #    run brgrs_direc1D.py -num_real 1 -itmax 180 -CFL 0.4 -Nx 250 -Nt 100 -typeJ "u" -dp "./../cases/data/2019_burger_data/"
+#    run brgrs_der_direc1D.py -num_real 1 -itmax 180 -CFL 0.4 -Nx 250 -Nt 100 -typeJ "u" -dp "./../cases/data/2019_burger_data/"
     parser = rib.parser()
     roe = rib.Class_Roe_BFGS(parser, call=True)
 
@@ -283,27 +180,41 @@ if __name__ == '__main__':
     #    X, y, utils = build_dataset(roe)
 
     print (" ")    
-        
-    input_shape = (roe.Nx-2, 1)
-    input_data = Input(shape=input_shape, name='u-input')
+    
+    der_u_input_shape = (roe.Nx-2, 1)  
+    u_input_shape = (roe.Nx-2, 1)
 
-    x = Conv1D(3, kernel_size=3, padding='same', name='first-convo')(input_data)
+    u_input_data = Input(shape=u_input_shape, name='u-input')
+    
+    der_u_input_data = Input(shape=der_u_input_shape, name='deru-input')
+
+    x = Conv1D(3, kernel_size=3, padding='same')(u_input_data)
     x = BatchNormalization()(x)
-    x = Activation('relu')(x)
-    print ("in {}--> Conv1D(3,3) = {}".format(input_shape, x.get_shape())) 
+    x = Activation('elu')(x)
 
-    in_pool_shape = np.shape(x)
-
-    x = Conv1D(3, kernel_size=3, padding='same', name='scd-convo')(x)
+    x = Conv1D(3, kernel_size=3, padding='same')(x)
     x = BatchNormalization()(x)
-    x = Activation('relu')(x)
-    print ("in {}--> Conv1D(3,3) = {}".format(in_pool_shape, x.get_shape())) 
+    x = Activation('elu')(x)
+    
+    print x.shape
 
-    to_concatene_first = x
+    deru = Conv1D(3, kernel_size=3, padding='same')(der_u_input_data)
+    deru = BatchNormalization()(deru)
+    deru = Activation('elu')(deru)
 
-    in_pool_shape = np.shape(x)
+    deru = Conv1D(3, kernel_size=3, padding='same')(deru)
+    deru = BatchNormalization()(deru)
+    deru = Activation('elu')(deru)
+    
+    print deru.shape
+    
+    xx = concatenate([x, deru])
+
+    to_concatene_first = xx
+
+    in_pool_shape = np.shape(xx)
      
-    x = AveragePooling1D(pool_size=2, padding='same',name='Max-convo')(x)
+    x = AveragePooling1D(pool_size=2, padding='same',name='Max-convo')(xx)
     print ("MaxPool1D pool_size = 2, padding same")
     print ("MaxPool1D(1st enc): in_shape = {}\tOut_shape = {}".format(in_pool_shape, x.get_shape()))
     print (" ")
@@ -315,14 +226,14 @@ if __name__ == '__main__':
 
     x = Conv1D(6, kernel_size=2, padding='same')(x)
     x = BatchNormalization()(x)
-    x = Activation('relu')(x)
+    x = Activation('elu')(x)
     print ("in {}--> Conv1D(3,3) = {}".format(in_pool_shape, x.get_shape())) 
 
     in_pool_shape = np.shape(x)
 
     x = Conv1D(6, kernel_size=2, padding='same')(x)
     x = BatchNormalization()(x)
-    x = Activation('relu')(x)
+    x = Activation('elu')(x)
     print ("in {}--> Conv1D(3,3) = {}".format(in_pool_shape, x.get_shape())) 
 
     to_concatene_second = x
@@ -338,21 +249,21 @@ if __name__ == '__main__':
 
     x = Conv1D(12, kernel_size=2, padding='same')(x)
     x = BatchNormalization()(x)
-    x = Activation('relu')(x)
+    x = Activation('elu')(x)
     print ("in {}--> Conv1D(3,3) = {}".format(in_pool_shape, x.get_shape())) 
 
     in_pool_shape = np.shape(x)
 
     x = Conv1D(12, kernel_size=2, padding='same')(x)
     x = BatchNormalization()(x)
-    x = Activation('relu')(x)
+    x = Activation('elu')(x)
     print ("in {}--> Conv1D(3,3) = {}".format(in_pool_shape, x.get_shape())) 
 
     in_pool_shape = np.shape(x)
 
     x = Conv1D(12, kernel_size=2, padding='same')(x)
     x = BatchNormalization()(x)
-    x = Activation('relu')(x)
+    x = Activation('elu')(x)
     print ("in {}--> Conv1D(3,3) = {}".format(in_pool_shape, x.get_shape())) 
 
     in_pool_shape = np.shape(x)
@@ -370,14 +281,14 @@ if __name__ == '__main__':
 
     x = Conv1D(6, kernel_size=2, padding='same')(y)
     x = BatchNormalization()(x)
-    x = Activation('relu')(x)
+    x = Activation('elu')(x)
     print ("in {}--> Conv1D(6,2) = {}".format(in_pool_shape, x.get_shape())) 
 
     in_pool_shape = np.shape(x)
 
     x = Conv1D(6, kernel_size=2, padding='same')(x)
     x = BatchNormalization()(x)
-    x = Activation('relu')(x)
+    x = Activation('elu')(x)
     print ("in {}--> Conv1D(6,2) = {}".format(in_pool_shape, x.get_shape())) 
 
     in_pool_shape = np.shape(x)
@@ -390,24 +301,22 @@ if __name__ == '__main__':
     y = concatenate([to_concatene_first, x])
 
     x = Conv1D(3, kernel_size=1, padding='same')(y)
-    x = Conv1D(1, kernel_size=3, padding='same')(x)
+    out = Conv1D(1, kernel_size=3, padding='same')(x)
 
     print ("Output shape = {}".format(np.shape(x)))
 
-
-
-
-    unet = Model(input_data, x, name='u_net_out')
+    unet = Model(inputs = [u_input_data, der_u_input_data], outputs=out, name='u_net_out')
         
     from keras.callbacks import TensorBoard
 
     adam = optimizers.Adam(lr=1e-2, decay=1e-4)
     unet.compile(loss='mean_squared_error', optimizer=adam)
 
-    X, y = build_dataset(roe)
-#    X = np.load("X_248.npy")
-#    y = np.load("y_248.npy")    
-
+#    X, y = build_dataset(roe)
+    X = np.load("Xder248.npy")
+    y = np.load("yder248.npy")    
+    d = np.load("dder248.npy")    
+    
     from sklearn.preprocessing  import StandardScaler
 
     scaler = StandardScaler()
@@ -417,22 +326,25 @@ if __name__ == '__main__':
 
     #    X_scaled = scaler.fit_transform(X)    
 
-    xtr, ytr, xte, yte, xval, yval = split_data(X, y)
+    xtr, ytr, dtr, xte, yte, dte, xval, yval, dval = split_data(X, d, y)
     
     x_train = np.reshape(xtr, (len(xtr), 248, 1))
     y_train = np.reshape(ytr, (len(ytr), 248, 1))
-
+    d_train = np.reshape(dtr, (len(ytr), 248, 1))
+    
     x_test = np.reshape(xte, (len(xte), 248, 1))
     y_test = np.reshape(yte, (len(yte), 248, 1))
+    d_test = np.reshape(dte, (len(yte), 248, 1))
 
     x_validation = np.reshape(xval, (len(xval), 248, 1))
     y_validation = np.reshape(yval, (len(yval), 248, 1))
-
-    unet.fit(x_train, y_train,
+    d_validation = np.reshape(dval, (len(yval), 248, 1))
+    
+    unet.fit(x=[x_train, d_train], y=y_train,
             epochs=10,
             batch_size=256,
             shuffle=True,
-            validation_data=(x_test, y_test),
+            validation_data=([x_test, d_test], y_test),
             callbacks=[TensorBoard(log_dir='/tmp/autoencoder2')])
 
     
